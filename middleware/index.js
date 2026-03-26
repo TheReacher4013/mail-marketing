58cef2686c97307c5ac3ec66258481f15e178e47
Soni2912@ <bansodesony48@gmail.com>
2026-03-26 11:10:19 +0530

    update

--- middleware/index.js
+++
@@ -1,5 +0,0 @@
-const { protect } = require('./authMiddleware');
-const { authorize, ROLES } = require('./roleMiddleware');
-const { uploadCSV } = require('./uploadMiddleware');
-
-module.exports = { protect, authorize, ROLES, uploadCSV };
