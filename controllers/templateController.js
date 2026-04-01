8cbb3cacf14f150749235378159fc9eb49333fa7
Soni2912@ <bansodesony48@gmail.com>
2026-04-01 15:42:05 +0530

    changed

--- controllers/templateController.js
+++ controllers/templateController.js
@@ -1,55 +1,8 @@
 const templateService = require('../services/templateService');
 const { sendSuccess, sendError } = require('../utils/responseHelper');
 
-const handleError = (res, error) => {
-  return sendError(res, error.message, error.status || 500);
-};
-
-
-exports.getAll = async (req, res) => {
-  try {
-    const templates = await templateService.getAll(req.query);
-    return sendSuccess(res, templates);
-  } catch (error) {
-    return handleError(res, error);
-  }
-};
-
-
-exports.getById = async (req, res) => {
-  try {
-    const template = await templateService.getById(req.params.id);
-    return sendSuccess(res, { template });
-  } catch (error) {
-    return handleError(res, error);
-  }
-};
-
-
-exports.create = async (req, res) => {
-  try {
-    const templateId = await templateService.create(req.body, req.user.id);
-    return sendSuccess(res, { templateId }, 'Template created.', 201);
-  } catch (error) {
-    return handleError(res, error);
-  }
-};
-
-exports.update = async (req, res) => {
-  try {
-    await templateService.update(req.params.id, req.body);
-    return sendSuccess(res, {}, 'Updated.');
-  } catch (error) {
-    return handleError(res, error);
-  }
-};
-
-
-exports.remove = async (req, res) => {
-  try {
-    await templateService.remove(req.params.id);
-    return sendSuccess(res, {}, 'Deleted.');
-  } catch (error) {
-    return handleError(res, error);
-  }
-};
+exports.getAll  = async (req, res) => { try { return sendSuccess(res, await templateService.getAll(req.query)); } catch (e) { return sendError(res, e.message, e.status||500); } };
+exports.getById = async (req, res) => { try { return sendSuccess(res, { template: await templateService.getById(req.params.id) }); } catch (e) { return sendError(res, e.message, e.status||500); } };
+exports.create  = async (req, res) => { try { const id = await templateService.create(req.body, req.user.id); return sendSuccess(res, { templateId: id }, 'Template created.', 201); } catch (e) { return sendError(res, e.message, e.status||500); } };
+exports.update  = async (req, res) => { try { await templateService.update(req.params.id, req.body); return sendSuccess(res, {}, 'Updated.'); } catch (e) { return sendError(res, e.message, e.status||500); } };
+exports.remove  = async (req, res) => { try { await templateService.remove(req.params.id); return sendSuccess(res, {}, 'Deleted.'); } catch (e) { return sendError(res, e.message, e.status||500); } };
--- controllers/trackingController.js
+++ controllers/trackingController.js
@@ -1,84 +1,40 @@
-const trackingService = require('../services/trackingService');
+const trackingService  = require('../services/trackingService');
 const { PIXEL_BUFFER } = require('../utils/trackingPixel');
 const { sendSuccess, sendError } = require('../utils/responseHelper');
 
-const handleError = (res, error, message = 'Something went wrong.') => {
-  return sendError(res, message, error?.status || 500);
-};
-
-
 exports.trackOpen = async (req, res) => {
   const { campaignId, contactId } = req.params;
-
-  try {
-    await trackingService.recordOpen(
-      campaignId,
-      contactId,
-      req.ip,
-      req.headers['user-agent']
-    );
-  } catch (error) {
-
-  }
-
- 
-  res.writeHead(200, {
-    'Content-Type': 'image/gif',
-    'Content-Length': PIXEL_BUFFER.length,
-    'Cache-Control': 'no-cache'
-  });
-
-  return res.end(PIXEL_BUFFER);
+  await trackingService.recordOpen(campaignId, contactId, req.ip, req.headers['user-agent']).catch(() => {});
+  res.writeHead(200, { 'Content-Type': 'image/gif', 'Content-Length': PIXEL_BUFFER.length, 'Cache-Control': 'no-cache' });
+  res.end(PIXEL_BUFFER);
 };
-
 
 exports.trackClick = async (req, res) => {
   const { campaignId, contactId } = req.params;
   const targetUrl = req.query.url;
-
-  if (!targetUrl) {
-    return res.status(400).send('Missing URL');
-  }
-
-  try {
-    await trackingService.recordClick(
-      campaignId,
-      contactId,
-      targetUrl,
-      req.ip
-    );
-  } catch (error) {
-    
-  }
-
+  if (!targetUrl) return res.status(400).send('Missing URL');
+  await trackingService.recordClick(campaignId, contactId, targetUrl, req.ip).catch(() => {});
   return res.redirect(decodeURIComponent(targetUrl));
 };
 
 exports.unsubscribe = async (req, res) => {
   const { campaignId, contactId } = req.params;
-
   try {
     await trackingService.recordUnsubscribe(campaignId, contactId);
-
-    return res.send(`
-      <html>
-        <body style="font-family:sans-serif;text-align:center;padding:60px">
-          <h2>You have been unsubscribed.</h2>
-          <p>You will no longer receive emails from us.</p>
-        </body>
-      </html>
-    `);
-  } catch (error) {
+    return res.send(`<html><body style="font-family:sans-serif;text-align:center;padding:60px">
+      <h2>You have been unsubscribed.</h2>
+      <p>You will no longer receive emails from us.</p>
+    </body></html>`);
+  } catch {
     return res.status(500).send('Something went wrong.');
   }
 };
-
 
 exports.getCampaignEvents = async (req, res) => {
   try {
     const events = await trackingService.getEvents(req.params.campaignId);
     return sendSuccess(res, { events });
-  } catch (error) {
-    return handleError(res, error, 'Failed to fetch events.');
+  } catch (err) {
+    return sendError(res, 'Failed.', 500);
   }
 };
